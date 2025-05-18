b4868e01f4951610f59a44d4504a1cabe351d96f
Harper <harperrcfinancial@gmail.com>
2025-05-18 02:22:45 -0500

    Delete sw.js

--- sw.js
+++
@@ -1,6 +0,0 @@
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open('payme-v1').then(cache => cache.addAll(['.', 'index.html', 'manifest.json', 'sw.js'])));
-});
-self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
-});
